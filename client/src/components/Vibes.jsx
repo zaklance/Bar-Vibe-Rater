7b5cd6aea78539bc850993ad3a40d25694b334db
zaklance <wosewick@gmail.com>
2024-07-18 14:02:57 -0400

    added radar graph

--- client/src/components/Vibes.jsx
+++ client/src/components/Vibes.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
+import Chart from 'chart.js/auto';
+import { useEffect } from "react";
 import '../index.css';
 
 function Vibes() {
@@ -7,21 +9,61 @@ function Vibes() {
 
     const tompkins = { lat: 40.72606737678102, lng: -73.98333751481684 }
 
-    return (
-        <>
-            <div id='map'>
-                <APIProvider apiKey={import.meta.env.VITE_GOOGLE_KEY} onLoad={() => console.log('Maps API has loaded.')}>
-                    <Map defaultCenter={tompkins} defaultZoom={13} map-id="4cdb05ac35ed1b8">
-                        <Marker position={tompkins} />
-                    </Map>
+    useEffect(() => {
+        const ctx = document.getElementById("marksChart");
+
+        const marksData = {
+            labels: ["Theme", "Atmosphere", "Libations", "Group Size", "Volume", "Food"],
+            datasets: [{
+                label: "Big Bar",
+                backgroundColor: "rgba(200,0,0,0.2)",
+                data: [4, 2, 4, 2, 3, 1]
+            }, {
+                label: "Tile Bar",
+                backgroundColor: "rgba(0,0,200,0.2)",
+                data: [4, 1, 2, 3, 4, 1]
+            }]
+        };
+        
+        const radarChart = new Chart(ctx, {
+            type: 'radar',
+            data: marksData,
+            options: {
+                scales: {
+                    r: {
+                        max: 5,
+                        min: 0,
+                        ticks: {
+                            display: false,
+                            stepSize: 1
+                        },
+                        grid: {
+
+                        }
+                    }
+                }
+            }
+        });
+    }, [])
+
+        return (
+            <>
+                <div id='map'>
+                    <APIProvider apiKey={import.meta.env.VITE_GOOGLE_KEY} onLoad={() => console.log('Maps API has loaded.')}>
+                        <Map defaultCenter={tompkins} defaultZoom={13} map-id="4cdb05ac35ed1b8">
+                            {/* <Marker position={tompkins} /> */}
+                        </Map>
+                    </APIProvider>
                     <gmp-map defaultCenter={tompkins} zoom={13} map-id="DEMO_MAP_ID">
                         <gmp-advanced-marker position={tompkins} title="Union Square"></gmp-advanced-marker>
                     </gmp-map>
-                </APIProvider>
-            </div>
-            <h2>Test</h2>
-        </>
-    )
-}
+                </div>
+                <h2>Test</h2>
+                <div className="vibe-chart">
+                    <canvas id="marksChart"></canvas>
+                </div>
+            </>
+        )
+    }
 
 export default Vibes;
---
+++ server/seed.py
@@ -0,0 +1,10 @@
+
+
+def run():
+
+
+
+
+# if __name__ == '__main__':
+#     with app.app_context():
+#         run()
